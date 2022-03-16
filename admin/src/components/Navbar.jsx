229bfa9210a91fe1b725649fd25a8a2633573d25
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-03-16 08:46:56 +0530

    normal changes

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -11,26 +11,26 @@ export default function Navbar() {
     return <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container">
-                <Link className="navbar-brand" to="/"><strong>ZangBox</strong></Link>
-                <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+            <Link className="navbar-brand" to="/"><strong>ZangBox</strong></Link>
+            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+                <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                <Link className="nav-link" to="/cart">
+                <button className="btn btn-light btn-sm">Cart
+                    <div className="badge text-dark">
+                        {
+                            cartItem.reduce((acc, item) => acc + (+item.qty), 0)
+                        }
+                    </div>
                 </button>
-                <div className="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/cart">
-                                <button className="btn btn-light btn-sm">Cart
-                                    <div className="badge text-dark">
-                                        {
-                                            cartItem.reduce((acc, item) => acc + (+item.qty), 0)
-                                        }
-                                    </div>
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                </Link>
+                </li>
+                </ul>
+            </div>
             </div>
         </nav>
     </div>;
