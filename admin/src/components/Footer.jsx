4f3fc761d8777bfc9a38e5c61cbb2bc7a43af9c6
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-03-17 14:04:55 +0530

    Footer changes

--- admin/src/components/Footer.jsx
+++ admin/src/components/Footer.jsx
@@ -4,34 +4,32 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <>
-        <footer className="text-center text-lg-start my-auto text-muted" style={{backgroundColor:"#89e657" }} >
-        <div className="me-5 d-none d-lg-block">
+        <footer style={{backgroundColor:"#89e657" }} >
+        <div>
             <span></span>
             </div>
-        <section className="mx-4 my-5" style={{marginTop: "10px"}}>
-            <div className="container text-center text-md-start mt-5">
-            <div className="row mt-3">      
-                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style={{paddingTop: "60px", color:"green"}}>
-                <h6 className="text-uppercase fw-bold mb-4">
+        <section style={{marginTop: "10px"}}>
+            <div>
+            <div style={{textAlign: "center",display: "flex",width: "100%"}}>      
+                <div style={{paddingTop: "60px", color:"green", margin:"auto"}}>
+                <h6 >
                     Contact
                 </h6>
-                <p><i className="fas fa-envelope me-3"></i>thezanglife@gmail.com</p>
-                <p><i className="fas fa-phone me-3"></i> + 91-8248890189</p>
+                <p><i></i>thezanglife@gmail.com</p>
+                <p><i></i> + 91-8248890189</p>
                 </div>
             
-                <div className="col-md-3 mx-auto mb-4" style={{paddingTop: "60px",  color:"green"}}>
-                
+                <div style={{paddingTop: "60px",  color:"green", margin:"auto"}}>
                 <p><Link to="/!" style={{color:"green"}}>Privacy and Refund policy</Link></p>
                 <p><Link to="/!" style={{color:"green"}}>Disclamer Policy</Link></p>
                 <p><Link to="/!" style={{color:"green"}}>Terms and Condition</Link></p>
                 <p><Link to="/!" style={{color:"green"}}>Click here for quick customer support</Link></p>
                 </div>
-            
             </div>
             </div>
         </section>
         
-        <div className="text-center">
+        <div style={{textAlign: "center"}}>
             © 2022 Copyright:
             <Link to="/TheSciFi.com" className='mx-2'>ZangBox.com</Link>
         </div>
