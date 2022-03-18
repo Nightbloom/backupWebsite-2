e4f296c13cafb6f4ce76180158567a9b9faaf572
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-03-18 10:23:06 +0530

    Final Design

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -1,32 +1,21 @@
 import React from "react";
-import "../style/Home.css";
-import plate from "../images/delicious-rice-chicken-meat-patties-with-garlic-tomato-sauce 1.png";
-import cook from "../images/cooking 1.png";
-import envelope from "../images/envelope 1.png";
-import foodDelivery from "../images/food-delivery (1) 1.png";
-import chilli from "../images/green-chili-peppers 1.png";
+import "./Home.css";
+import mask1 from "../images/pan.png";
 import mask2 from "../images/Mask group-1.png";
 import mask3 from "../images/Mask group-2.png";
 import mask4 from "../images/Mask group.png";
+import cook from "../images/cooking 1.png";
+import plate from "../images/delicious-rice-chicken-meat-patties-with-garlic-tomato-sauce 1.png";
+import foodDelivery from "../images/food-delivery (1) 1.png";
+import chilli from "../images/green-chili-peppers 1.png";
 import bike from "../images/motorbike 1.png";
-import phone from "../images/phone 1.png";
 import pepper from "../images/top-view-bell-pepper-pepper-sliced-white-horizontal-2 1.png";
 import wicon from "../images/wicon.png";
-import mask1 from "../images/pan.png";
-import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div className="conatainer">
-      <NavBar />
       <div className="header">
-        <nav>
-          <div className="logo">Zang</div>
-          <div className="menu">
-            <ul className="menu-group">
-              <li className="list-item">Home</li>
-            </ul>
-          </div>
-        </nav>
         <div className="zangHeading">Zang It</div>
       </div>
       <div className="maskgroup">
@@ -53,38 +42,53 @@ export const Home = () => {
         <img src={wicon} />
       </div>
       <div className="welcome">
-        <div className="sub_heading">Welcome to the</div>
+        <div className="sub_heading">
+          Hassle free and exotic cooking in less than 30 minutes
+        </div>
         <div className="welcome_content">
           <div className="welcome_Section1">
             <img src={plate} />
           </div>
           <div className="welcome_Section2">
-            <div className="main_heading">ZangBox Provider</div>
             <p className="sub_para">
-              We Provide you Traditional, Healthy and Exotic recipes that reduce
-              your effort on cooking and are easy to cook under 6 steps in less
-              than 30 minutes. We also offer you exotic, traditional and healthy
-              culsines on special ccasios to make memorable memories.
+              We proviude you delicious, exotic and healthy food meals with
+              fresh ingredients of high quality, curated by the best chefs in
+              the industry
             </p>
           </div>
         </div>
       </div>
       <div className="footer">
         <div className="heading3">How it Works</div>
-        <div className="foodDelivery">
+        <div className="bike">
           <img src={foodDelivery} />
         </div>
         <div className="footer_heading">Choose Your Zangbox</div>
         <p className="footer_content">
           We offer a wide range of exotic cuisine keeping in mind both your
-          cravings and healt
+          cravings and health.
         </p>
         <div className="bike">
           <img src={bike} />
         </div>
-        <div>
-          <button className="orderBtn">Order Now</button>
+        <div className="footer_heading">Get Your Zangbox Delivered</div>
+        <p className="footer_content">
+          You would be recieving healthy and premium quality ingredients in an
+          eco-friendly packaging.
+        </p>
+        <div className="bike">
+          <img src={cook} />
         </div>
+        <div className="footer_heading">Bring Soul to your Food</div>
+        <p className="footer_content_last">
+          Follow the tutorials given by us, cook amazing meals to create
+          wonderful memories with your faimly.
+        </p>
+      </div>
+      <div>
+        <Link to="/product" className="orderBtn">
+          Next
+        </Link>
       </div>
     </div>
   );
--- admin/src/pages/Products.jsx
+++ admin/src/pages/Products.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAllProductAction } from "../actions/product-action";
 import ProductCard from "./../components/ProductCard";
 
-export default function Home() {
+export default function Products() {
   const dispatch = useDispatch();
   const { reduxProduct, isLoading } = useSelector((state) => state.product);
   useEffect(() => {
